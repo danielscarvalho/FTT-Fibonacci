97dd7f2d3311df919033141928808aabeaffbb45
Daniel Carvalho <danielscarvalho@gmail.com>
2019-02-28 01:09:50 -0300

    fix...

--- src/br/edu/cefsa/ftt/ec/Main.java
+++ src/br/edu/cefsa/ftt/ec/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {   
     	
     Fibonacci fibo = new Fibonacci();
-    
 	     
     for (int i = 0; i < 40; i++) {
             System.out.println(+ i + ": " + fibo.getFibonacci(i) );
