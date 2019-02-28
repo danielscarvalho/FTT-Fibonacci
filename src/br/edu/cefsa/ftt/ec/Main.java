271da5d5ba4eaaeeab7eba4cac5302b588a191c1
Daniel Carvalho <danielscarvalho@gmail.com>
2019-02-28 01:02:00 -0300

    First code...

--- src/br/edu/cefsa/ftt/ec/Main.java
+++ src/br/edu/cefsa/ftt/ec/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {   
     	
     Fibonacci fibo = new Fibonacci();
+    
 	     
     for (int i = 0; i < 40; i++) {
             System.out.println(+ i + ": " + fibo.getFibonacci(i) );
